f6bc0d986a023d5dfb7e5c97658d38fb4f82f8d8
mdjibo2 <justpourmestest00@gmail.com>
2022-10-06 19:10:58 +0000

    Correction

--- pchar.c
+++ pchar.c
@@ -12,20 +12,21 @@
  * Return: void
  */
 void pchar(stack_t **stack, unsigned int line_cnt)
-
 {
+
 	if (!stack || !(*stack))
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_cnt);
 		exit(EXIT_FAILURE);
 		return;
-
-/*if (isblank((*stack)->n))*/
-	/*{*/
-	/*fprintf(stderr, "L%d: can't pchar, value out of range\n", line_cnt);*/
-		/*exit(EXIT_FAILURE);*/
-		/*return;*/
-	/*}*/
+	}
+/*	if (isascii((*stack)->n) == 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_cnt);
+		exit(EXIT_FAILURE);
+		return;
+	}
+*/
 	printf("%c\n", (*stack)->n);
 }
 
